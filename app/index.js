faa131e7e3037bb1a4967cd4fd1d2cba76517dd5
Shivanshudeveloper <developer.shivanshu@gmail.com>
2023-03-23 23:45:10 +0530

    Changes

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const mustache = require('mustache');
 const csv = require('csv-parser');
 const fs = require('fs');
 
-const servers = ["a15d1b45-97da-4087-8ded-da11ec0311de", "91544462-f272-4b57-b119-84eeffd31530"];
+const servers = ["91544462-f272-4b57-b119-84eeffd31530", "eacbc1f9-3eed-445d-837a-56fd3ffb5e56", "f6a9551c-2a98-440c-bbb2-da5498d4058f", "f90885e1-4839-4c9c-be23-d72ee170e0cf", "bbdf4a86-13e8-44d2-af27-1d74274e8608", "fbfe19e0-75df-4a74-8df6-154dcb8e3594", "599b697b-02b2-48f5-8b84-e0465e19ac32", "ecacd9d4-1df0-4b06-8fb0-0d3deecbcffc", "158c7988-c349-46fd-80f0-58652530b834", "7ae69de2-4855-4e75-8e4f-1f4ac929274f", "efb80930-1c79-427b-8999-f71c90f29427"];
 
 
 function delay(ms) {
@@ -18,10 +18,17 @@ function delay(ms) {
 
 
 async function processData() {
+  var count = 0;
   return new Promise((resolve) => {
     fs.createReadStream('main.csv')
       .pipe(csv())
       .on('data', async (data) => {
+        if (count === servers.length) {
+          count = 0;
+        }
+        var serverKey = servers[count];
+        count = count + 1;
+
         const template = fs.readFileSync('./templates/template.html', 'utf8');
 
         const { firstName, email } = data;
@@ -31,10 +38,10 @@ async function processData() {
         const html = mustache.render(template, emailData);
 
         // Send an email:
-        var client = new postmark.ServerClient("a15d1b45-97da-4087-8ded-da11ec0311de");
+        var client = new postmark.ServerClient(serverKey);
 
 
-        const from = "marvin@nftbrands-inc.com";
+        const from = "josh@nftbrands-inc.com";
 
         const to = email;
 
